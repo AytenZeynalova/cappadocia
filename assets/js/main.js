c45c2fbf0780e07c8709edf7da73508441b16f22
AytenZeynalova <14rvxpg@code.edu.az>
2022-12-01 02:20:58 +0400

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+const hotelimages=[
+    {
+        id:1,
+        imageSrc:"./assets/img/Rectangle 25.png"
+    },
+    {
+        id:2,
+        imageSrc:"./assets/img/Rectangle 31.png"
+    },
+    {
+        id:3,
+        imageSrc:"./assets/img/Rectangle 31.png"
+    },
+    {
+        id:4,
+        imageSrc:"./assets/img/Rectangle 32.png"
+    }
+]
+
+
+
+
+
+
+
+// const HotelPics = document.querySelector(".hotel-pics-1")
+
+
+// for (const images of hotelimages) {
+//     renderImagesCard(images)
+// }
+
+// function renderImagesCard(images) {
+//   const newElement = document.createElement("div")
+//   HotelPics.prepend(newElement)
+//   newElement.outerHTML = getImagesMarkup(images)
+// }
+
+// function getImagesMarkup(images) 
+// {
+//   return
+//       <div class="pics">
+//         <img src="${images.imageSrc}" alt="">
+//       </div>
+  
+
+// }
